53951a50f004a2ceff18e611551881a186bc3900
Eddy-star223 <eddyeffanga@gmail.com>
2025-04-20 20:36:37 +0100

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,29 @@
+
+public class Table {
+  public static void main(String[] args) {
+ 
+
+int a = 5;
+int b = 5;
+int sum = a + b; 
+
+System.out.println("Table display:");
+
+for (int i = 1; i < a; i++) {
+for (int j = 2; j < b; j++) {
+
+System.out.print(i * j + "  ");
+
+}
+System.out.println();
+}
+
+float number1 = 13.81f;
+int number2 = (int) number1;
+
+System.out.println(number2);
+
+}
+}
+
+
